a2106e409fff615894fbdf319b37b47f47a34a09
Sean Morris <640101+seanmorris@users.noreply.github.com>
2025-09-27 17:15:56 -0400

    Documenting.

--- helpers/navbar.php
+++ helpers/navbar.php
@@ -89,7 +89,7 @@ function makeNavBar($path, $rootPath = NULL)
 		$frontmatter = $entry->frontmatter;
 
 		$title = $frontmatter['title'] ?? ucwords(preg_replace(['/\.md$/', '/-/'], ['',  ' '], $filename));
-		$linkPath = preg_replace('/\.\w+$/', '.html', substr($pathname, strlen($rootPath)));
+		$linkPath = '.' . preg_replace('/\.\w+$/', '.html', substr($pathname, strlen($rootPath)));
 
 		?><li>
 			<a href = "<?=$linkPath;?>" <?= getenv('CURRENT_PAGE') === $linkPath ? 'class="active-link"' : ''; ?>>
